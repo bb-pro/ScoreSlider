5c4151ba39378f548cb9b85c523d2dd59b9d20a0
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-05-02 12:54:11 +0500

    Initial Commit

---
+++ ScoreSlider/ScoreSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreSliderApp.swift
+//  ScoreSlider
+//
+//  Created by Bektemur Mamashayev on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ScoreSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
